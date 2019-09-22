d1ce2b3a65de4f14d13d7cddc2c720390ef2d94a
손희승 <sonhees99@naver.com>
2019-09-22 18:11:17 +0900

    .

--- Project2/Project2/IIkH.h
+++ Project2/Project2/IIkH.h
@@ -1,13 +1,16 @@
 #pragma once
 #include <iostream>
+#include <set>
 #include <vector>
+#include <string>
 
 class Greeter {
 private:
 	PlanManager & proc;
 	DataBaseManager & DB;
 public:
-	Greeter(PlanManager & p, DataBaseManager & db) : proc(p), DB(db) {};
+	Greeter(PlanManager & p, DataBaseManager & db) :
+		proc(p), DB(db) {};
 	void Init();
 	void Menu();
 	void SubMenu(int i);
@@ -15,28 +18,61 @@ public:
 
 class PlanManager {
 private:
-	struct Date {
-		DataBaseManager::Meal meal;
+	class Date {
+	private:
+		enum { BREAKFAST = 0, LAUNCH, DINNER };
+		int year, month, day;
+		DataBaseManager::Meal meals[3];
+
+	public:
+		Date(int y, int m, int d) :
+			year(y), month(m), day(d) {
+		};
+		void DisplayAndEdit();
+		void BuildGroceryList( DataBaseManager::Meal * m);
+		friend bool operator<(const Date& first, const Date& second) {
+			if (first.year < second.year) return true;
+			if (first.year > second.year) return false;
+			if (first.month < second.month) return true;
+			if (first.month > second.month) return false;
+			if (first.day < second.day) return true;
+			return false;
+		}
 	};
-	std::vector<Date> dates;
+	std::set<Date> dates;
 
 public:
 	PlanManager() {};
+
 };
 
 class DataBaseManager {
 private:
-	struct Meal {
+	class Recipe {
+	private:
+		std::string name;
+		std::string description;
+	public:
 
 	};
 
-	struct Recipe {
-
+	class Meal {
+	private:
+		std::string name;
+		std::vector<int> rec;
+	public:
+		Meal(std::string&& n = std::string("")) :
+			name(n), rec(5) {};
+		void addRecipe(Recipe& r);
+		friend bool operator<(const Meal& first, const Meal& second) {
+			return first.name < second.name;
+		}
 	};
 
-	std::vector<Meal> meals;
+	std::set<Meal> meals;
 	std::vector<Recipe> recipes;
 
 public:
 	friend PlanManager;
+
 };
